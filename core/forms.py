eb64a13ace101aa2c2e10b962359c29ea7dc8a64
Paul Cappaert <paul.cappaert@gmail.com>
2020-02-21 11:45:47 -0500

    basic user pages

---
+++ core/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError(u'Email addresses must be unique.')
+        return super().clean()
--- core/views.py
+++ core/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'user/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Account updated')
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)
+    return render(request, 'core/profile.html', {'form': form})
